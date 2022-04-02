3c1031e5a38364a8185b1471b2274ed90c0daaaa
alonlevy5 <alonlevy5@gmail.com>
2022-04-02 21:30:27 +0300

    Added connection to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const dotenv = require("dotenv").config()
 const express = require('express')
 const app = express()
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error => {console.error(error)})
+db.once('open', ()=> {console.log('Connected to Mongo Succesfully!')})
+
 const port = process.env.PORT
 
 const indexRouter = require('./routes/index')
