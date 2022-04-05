f699d8d34b3ff15d6c61ceaee6284b41e9232112
alonlevy5 <alonlevy5@gmail.com>
2022-04-05 19:49:40 +0300

    Implemented Swagger with full Task API Support

--- routes/task_routes.js
+++ routes/task_routes.js
@@ -2,8 +2,98 @@ const express = require('express')
 const router = express.Router()
 
 const Task = require('../controllers/task')
+
+/**
+* @swagger
+* tags:
+*   name: Task Api
+*   description: The Task API
+*/
+
+/**
+* @swagger
+* components:
+*   schemas:
+*     Task:
+*       type: object
+*       required:
+*         - message
+*         - amount
+*       properties:
+*         message:
+*           type: string
+*           description: The task's text. 
+*         amount:
+*           type: number
+*           description: The amount you get for the given task.
+*       example:
+*         message: 'Do your homework'
+*         amount: 150
+*/
+
+/**
+* @swagger
+* /task:
+*   get:
+*     summary: Get all tasks
+*     tags: [Task Api]
+*     responses:
+*       200:
+*         description: The task list
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Task'
+*/
 router.get('/', Task.getTasks)
+
+/**
+* @swagger
+* /task/{id}:
+*   get:
+*     summary: Get task by ID
+*     tags: [Task Api]
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*         required: true
+*         description: The task id
+*     responses:
+*       200:
+*         description: The task list
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Task'
+*/
 router.get('/:id', Task.getTasksById)
+
+/**
+* @swagger
+* /task:
+*   post:
+*     summary: Add new task
+*     tags: [Task Api]
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             $ref: '#/components/schemas/Task'
+*     responses:
+*       200:
+*         description: The new task
+*         content:
+*           application/json:
+*             schema:
+*               $ref: '#/components/schemas/Task'
+*/
 router.post('/', Task.addTasks)
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,8 +4,29 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+if (process.env.NODE_ENV == "development") {
+    const swaggerUI = require("swagger-ui-express")
+    const swaggerJsDoc = require("swagger-jsdoc")
+    const options = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "WizeWallet Backend API's",
+                version: "1.0.0",
+                description: "Express Library API's",
+            },
+            servers: [{url: "http://localhost:" + process.env.PORT,},],
+        },
+        apis: ["./routes/*.js"],
+    };
+    const specs = swaggerJsDoc(options);
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+ }
+
+
 app.use(bodyParser.urlencoded({ extended: true, limit: '1mb'}))
 app.use(bodyParser.json())
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', error => {console.error(error)})
