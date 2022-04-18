c6d39b6e9b3d7b068aff76721a6ce1a3413a14c1
Maor <marmaor@gmail.com>
2022-04-18 12:24:39 +0300

    Created childSchema

---
+++ models/child_model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const childSchema = new mongoose.Schema(
+  {
+    _id: String,
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    tokens: {
+      type: [String],
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+
+    parents: {
+      type: [String],
+    },
+    transactions: [
+      {
+        description: String,
+        amount: Number,
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Child", childSchema);
